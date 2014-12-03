432ff4b2ec5063ccc10b632e776287a6e97ab1f8
Marianna Rapoport <mrapoport@uwaterloo.ca>
2014-12-03 10:39:30 -0500

    moving more stuff to appendix

--- code/assertions.java
+++ code/assertions.java
@@ -2,15 +2,12 @@
     String n = "not secret";
     notSecret(n);  // assert that n is not secret
     String s1 = f(n);
-    isSecret(s1);  // in the next statement, f is invoked with a secret value
-                   // hence, the argument of f will always be considered secret
-                   // and f will always return a secret value
+    isSecret(s1);
     String s2 = f(secret());
   }
 
   static String f(String str) {
-    isSecret(str); // the function is once invoked with a secret value
-                   // hence, assert that str is secret
+    isSecret(str);
     return str;
   }
   
