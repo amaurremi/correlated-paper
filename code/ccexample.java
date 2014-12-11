196067fe5e1e8b0d1f8d0e6131618fafac8cb2e1
ftip <ftip@samsung.com>
2014-12-11 15:12:07 -0800

    wrote new section 2

--- code/ccexample.java
+++ code/ccexample.java
@@ -1,25 +1,16 @@
 class A {
-  String foo {
-    return secret();
-  }
-  
-  void bar(String s) {}
+  String foo { return secret(); } /*@\label{line:Afoo}@*/
+  void bar(String s) {} /*@\label{line:Abar}@*/
 }
-
 class B extends A {
-  String foo {
-    return "not secret";
-  }
-  
-  void bar(String s) {
-    System.out.println(s);
-  }
+  String foo { return "not secret"; } /*@\label{line:Bfoo}@*/
+  void bar(String s) { System.out.println(s); } /*@\label{line:Bbar}@*/
 }
 
 class Main {
   public static void main(String[] args) {
-    A a = args == null ? new A() : new B();
-    String v = a.foo();
-    a.bar(v);
+    A a = (args == null) ? new A() : new B(); /*@\label{line:createObject}@*/
+    String v = a.foo(); /*@\label{line:callfoo}@*/
+    a.bar(v); /*@\label{line:callbar}@*/
   }
 }
