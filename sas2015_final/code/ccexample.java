02b229212c8600ebe5aa3fef591f1fe9f64c6d32
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-06-19 19:05:29 -0400

    creating final version without comments and redundant files

---
+++ sas2015_final/code/ccexample.java
@@ -0,0 +1,21 @@
+class A {
+  String foo { return secret(); } /*@\label{line:Afoo}@*/
+  void bar(String s) {} /*@\label{line:Abar}@*/
+}
+class B extends A {
+  String foo {
+    return "not secret";
+  } /*@\label{line:Bfoo}@*/
+  void bar(String s) {
+    System.out.println(s); /*@\label{line:Bbar}@*/
+  } 
+}
+
+class Main {
+  static void main(String[] args) {
+    A a = (args == null) /*@\label{line:createObject}@*/
+        ? new A() : new B(); 
+    String v = a.foo(); /*@\label{line:callfoo}@*/
+    a.bar(v); /*@\label{line:callbar}@*/
+  }
+}
